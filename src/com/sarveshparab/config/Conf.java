d936aaec1aa81f73daa3dd1d60d500e04f6d2d83
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-03 18:01:31 -0800

    conf updated

--- src/com/sarveshparab/config/Conf.java
+++ src/com/sarveshparab/config/Conf.java
@@ -1,10 +1,10 @@
 package com.sarveshparab.config;
 
-public class conf {
+public class Conf {
 
     public static final String ZK_PRE_CODE_BASE_PATH = "src/com/sarveshparab/subjectsystem/pre/sourcecode/src/java/main";
     public static final String ZK_POST_CODE_BASE_PATH = "src/com/sarveshparab/subjectsystem/post/sourcecode/src/java/main";
 
-
+    public static final String FILE_EXT = ".java";
 
 }
