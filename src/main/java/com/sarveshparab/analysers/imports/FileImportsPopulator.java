ebe34494c1ffa78a9c3cb1a656f773782e01a2a9
ameyakulkarni <abkulkar@usc.edu>
2019-12-03 18:52:42 -0800

    added functionality to read file using correct modified path

--- src/main/java/com/sarveshparab/analysers/imports/FileImportsPopulator.java
+++ src/main/java/com/sarveshparab/analysers/imports/FileImportsPopulator.java
@@ -1,4 +1,24 @@
 package com.sarveshparab.analysers.imports;
 
+import com.sarveshparab.util.FileHandler;
+import com.sarveshparab.util.StringManipulator;
+import com.sarveshparab.config.Conf;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class FileImportsPopulator {
+    public static HashMap<String, List<String>> createFileImportMap(List<String> fileList){
+        HashMap<String,List<String>> fileMap=new HashMap<>();
+
+        for(String filePath: fileList) {
+            String actualpath = Conf.ZK_PRE_CODE_BASE_PATH +
+                    StringManipulator.packagePathToSysPath(filePath, "/") + Conf.FILE_EXT;
+            String data = FileHandler.readLineByLine(actualpath);
+
+        }
+
+        return fileMap;
+    }
+
 }
