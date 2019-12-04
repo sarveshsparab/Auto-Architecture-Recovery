486af2dfc6a29705760a39f553958618e0c875ec
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-03 17:52:34 -0800

    ZK pre & post src paths added

--- src/com/sarveshparab/config/conf.java
+++ src/com/sarveshparab/config/conf.java
@@ -2,6 +2,9 @@ package com.sarveshparab.config;
 
 public class conf {
 
-    public static final String ZK_PRE_BASE_PATH = "";
+    public static final String ZK_PRE_CODE_BASE_PATH = "src/com/sarveshparab/subjectsystem/pre/sourcecode/src/java/main";
+    public static final String ZK_POST_CODE_BASE_PATH = "src/com/sarveshparab/subjectsystem/post/sourcecode/src/java/main";
+
+
 
 }
