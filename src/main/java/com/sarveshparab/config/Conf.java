1ba628e4d2c7301e81c0904c60e6071cfb093e27
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-05 11:37:54 -0800

    ZK build path

--- src/main/java/com/sarveshparab/config/Conf.java
+++ src/main/java/com/sarveshparab/config/Conf.java
@@ -5,6 +5,9 @@ public class Conf {
     public static final String ZK_PRE_CODE_BASE_PATH = "src/main/resources/subjectsystem/pre/sourcecode/src/java/main/";
     public static final String ZK_POST_CODE_BASE_PATH = "src/main/resources/subjectsystem/post/sourcecode/src/java/main/";
 
+    public static final String ZK_PRE_BUILD_PATH = "src/main/resources/subjectsystem/pre/sourcecode/build/";
+    public static final String ZK_POST_BUILD_PATH = "src/main/resources/subjectsystem/post/sourcecode/build/";
+
     public static final String FILE_EXT = ".java";
 
     public static final String BANNED_PACKAGE_PATH="src/main/java/com/sarveshparab/domain/banned.txt";
