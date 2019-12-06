c73caf633fc82b5fafc3915bf1c61af9f0affc71
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-06 02:22:31 -0800

    command cntral updated

--- src/main/java/com/sarveshparab/primary/CommandCentral.java
+++ src/main/java/com/sarveshparab/primary/CommandCentral.java
@@ -5,12 +5,8 @@ import com.sarveshparab.clustering.SecurityClustering;
 import com.sarveshparab.config.Conf;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class CommandCentral {
-
-
 
     public CommandCentral() {
     }
@@ -20,17 +16,10 @@ public class CommandCentral {
         SecurityClustering securityClustering = new SecurityClustering();
 
         smellAnalyser.runSmellAnalyser(Conf.ZK_PRE_SMELL_FILE);
-        Set<String> smellyFiles = smellAnalyser.getSmellyFiles();
-//
-//        smellyFiles.forEach(System.out::println);
-
-
-//        smellyFiles = smellyFiles.stream().filter(s->!s.contains("org.apache.zookeeper.cli.")).collect(Collectors.toSet());
-
-        List<String> secFiles = securityClustering.generateCluster(smellyFiles);
+        List<String> secFiles = securityClustering.generateCluster(smellAnalyser.getSmellyFiles());
 
         System.out.println(secFiles);
+        System.out.println(secFiles.size());
     }
-
 
 }
