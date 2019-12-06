8575c785224007ab8775f16722aefb2d0c400366
sonali856 <gujarath@usc.edu>
2019-12-05 20:06:33 -0800

    - Added reading and writing list to file logic

--- src/main/java/com/sarveshparab/util/FileHandler.java
+++ src/main/java/com/sarveshparab/util/FileHandler.java
@@ -1,9 +1,14 @@
 package com.sarveshparab.util;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class FileHandler {
@@ -23,5 +28,28 @@ public class FileHandler {
     public static String[] splitFileLineByLine(String fileContent){
         String returnArray[]=fileContent.split("\n");
         return returnArray;
+    }
+
+    public static void writeListToFile(String filePath, List<String> stringList) {
+
+        Path out = Paths.get(filePath);
+        try {
+            Files.write(out,stringList, Charset.defaultCharset());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static List<String> readListFromFile(String filePath){
+
+        List<String> lines = new ArrayList<>();
+        try {
+         lines = Files.readAllLines(new File(filePath).toPath(), Charset.defaultCharset() );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
     }
 }
