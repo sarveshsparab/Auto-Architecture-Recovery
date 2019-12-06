87e14168a128ce06a2bc9994bcd21f1b7fafcc66
sonali856 <gujarath@usc.edu>
2019-12-05 19:39:47 -0800

    - added levenstein in similarity algos

--- src/main/java/com/sarveshparab/analysers/semantic/similarity/SentenceSimilarityAlgorithms.java
+++ src/main/java/com/sarveshparab/analysers/semantic/similarity/SentenceSimilarityAlgorithms.java
@@ -5,7 +5,9 @@ import info.debatty.java.stringsimilarity.*;
 
 public class SentenceSimilarityAlgorithms {
 
+    public SentenceSimilarityAlgorithms(){
 
+    }
 
     public double cosineSimilarity(String string1,String string2){
         Cosine cosineObeject = new Cosine();
@@ -22,18 +24,21 @@ public class SentenceSimilarityAlgorithms {
         return jaroWinklerObject.similarity(string1,string2);
     }
 
+    public double levensteinSimilarity(String string1,String string2){
+        Levenshtein levenshteinObject = new Levenshtein();
 
-
-
+        return (1/levenshteinObject.distance(string1,string2));
+    }
 
 
     public double avgSimilarity(String string1,String string2){
 
         double sum = cosineSimilarity(string1,string2) +
                 jaccardSimilarity(string1,string2)+
-                jaroWinklerSimilarity(string1,string2);
+                jaroWinklerSimilarity(string1,string2)+
+                levensteinSimilarity(string1,string2);
 
 
-        return sum/3;
+        return sum/4;
     }
 }
