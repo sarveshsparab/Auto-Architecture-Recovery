10939712d0590167e3af9264de34b3eb3b078a25
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-06 21:50:07 -0800

    The main function and primary module!

--- src/main/java/com/sarveshparab/primary/CommandCentral.java
+++ src/main/java/com/sarveshparab/primary/CommandCentral.java
@@ -1,6 +1,8 @@
 package com.sarveshparab.primary;
 
 import com.sarveshparab.analysers.classfile.ReflectionBasedAnalyser;
+import com.sarveshparab.analysers.domain.DomainAnalyser;
+import com.sarveshparab.analysers.domain.MatchType;
 import com.sarveshparab.analysers.git.CommitContent;
 import com.sarveshparab.analysers.git.GitCommitAnalyser;
 import com.sarveshparab.analysers.imports.FileImportsPopulator;
@@ -17,9 +19,12 @@ import java.util.*;
 public class CommandCentral {
 
     private static SemanticAnalyser semanticAnalyser=new SemanticAnalyser();
-    private static ReflectionBasedAnalyser reflectionBasedAnalyser = new ReflectionBasedAnalyser(semanticAnalyser,true);
+    private static ReflectionBasedAnalyser preReflectAnalyser = new ReflectionBasedAnalyser(semanticAnalyser,true);
+    private static ReflectionBasedAnalyser postReflectAnalyser = new ReflectionBasedAnalyser(semanticAnalyser,false);
     private static SmellAnalyser smellAnalyser = new SmellAnalyser();
-    private static SecurityClustering securityClustering = new SecurityClustering(semanticAnalyser, reflectionBasedAnalyser);
+    private static DomainAnalyser domainAnalyser = new DomainAnalyser(semanticAnalyser);
+    private static SecurityClustering securityClustering = new SecurityClustering(semanticAnalyser,
+            preReflectAnalyser, domainAnalyser);
     private static FileImportsPopulator fileImportsPopulator = new FileImportsPopulator();
     private static GitCommitAnalyser gitCommitAnalyser = new GitCommitAnalyser();
 
@@ -28,18 +33,20 @@ public class CommandCentral {
 
     public static void main(String args[]) {
 
+        // Smelly file analysis
         Set<String> smellyFiles = null;
         if (Params.LOAD_SMELL_ANALYSIS && FileHandler.doesFileExists(Conf.SMELLY_ANALYSIS_DUMP)){
             smellyFiles = FileHandler.readSetFromFile(Conf.SMELLY_ANALYSIS_DUMP);
         } else {
             smellAnalyser.runSmellAnalyser(Conf.ZK_PRE_SMELL_FILE);
             smellyFiles = smellAnalyser.getSmellyFiles();
+            FileHandler.writeSetToFile(Conf.SMELLY_ANALYSIS_DUMP, smellyFiles);
         }
 
-        FileHandler.writeSetToFile(Conf.SMELLY_ANALYSIS_DUMP, smellyFiles);
+        // Build security related files cluster
+        List<String> secFiles = securityClustering.generateCluster(smellyFiles);
 
-        List<String> secFiles = securityClustering.generateCluster(smellyFiles, true);
-
+        // Analyse import statements across versions of subject system
         Set<String> importsDiff = new HashSet<>();
         for (String secFile : secFiles) {
 
@@ -51,11 +58,12 @@ public class CommandCentral {
         }
 
         importsDiff.addAll(secFiles);
-
         FileHandler.writeSetToFile(Conf.SECURITY_CLUSTER_DUMP_WITH_IMPORTS_DIFF, importsDiff);
 
+        // Build git commit analysis on security cluster files
         Map<String, CommitContent> commitContentMap = null;
         if (Params.LOAD_COMMIT_ANALYSIS && FileHandler.doesFileExists(Conf.COMMIT_CONTENT_DUMP)) {
+            System.out.println("Loaded Pre-analyzed Commit files");
             commitContentMap = FileHandler.loadFromJson(Conf.COMMIT_CONTENT_DUMP);
         } else {
             gitCommitAnalyser.gitRemoteClone(Conf.ZK_REMOTE_GIT_URL, Conf.ZK_REMOTE_GIT_REPO);
@@ -81,6 +89,70 @@ public class CommandCentral {
 
             gitCommitAnalyser.deleteGitRemoteClone(Conf.ZK_REMOTE_GIT_REPO);
         }
+
+        // Post git-commit analysis
+        Map<String, CommitContent> commitContentSecThresholdMap = new HashMap<>();
+        Map<String, CommitContent> commitContentSecMsgMap = new HashMap<>();
+
+        if (Params.LOAD_POST_GIT_ANALYSIS && (FileHandler.doesFileExists(Conf.COMMIT_CONTENT_BREACHING_THRESHOLD)
+                && FileHandler.doesFileExists(Conf.COMMIT_CONTENT_MSG_BREACHING_THRESHOLD))){
+            commitContentSecThresholdMap = FileHandler.loadFromJson(Conf.COMMIT_CONTENT_BREACHING_THRESHOLD);
+            commitContentSecMsgMap = FileHandler.loadFromJson(Conf.COMMIT_CONTENT_MSG_BREACHING_THRESHOLD);
+        } else {
+            for (Map.Entry<String, CommitContent> entry : commitContentMap.entrySet()) {
+                CommitContent cc = entry.getValue();
+
+                // Consider commits only with more than a threshold of security related files
+                if (cc.getSecFilesCount() > Params.COMMIT_FREQ_THRESHOLD) {
+                    commitContentSecThresholdMap.put(cc.getId(), cc);
+                    HashMap<Enum<MatchType>, List<String>> matchedWords = domainAnalyser.returnMatches(cc.getAnalysedMsg());
+
+                    // Consider commits only whose commit message has a certain amount of security related words
+                    if (matchedWords.get(MatchType.EXACT).size() >= Params.POST_COMMIT_EMATCH_THRESHOLD) {
+                        commitContentSecMsgMap.put(cc.getId(), cc);
+                    }
+                }
+            }
+
+            FileHandler.saveToJson(Conf.COMMIT_CONTENT_BREACHING_THRESHOLD, commitContentSecThresholdMap);
+            FileHandler.saveToJson(Conf.COMMIT_CONTENT_MSG_BREACHING_THRESHOLD, commitContentSecMsgMap);
+        }
+
+        if (Params.COMPUTE_AFFECTED_FILES_STATS){
+            Map<String, Map<Enum<MatchType>, Integer>> commitMatchTypeFreq = new HashMap<>();
+            for (Map.Entry<String, CommitContent> entry : commitContentSecMsgMap.entrySet()){
+                CommitContent cc = entry.getValue();
+
+                Map<Enum<MatchType>, Integer> matchTypeFreq = new HashMap<>();
+                int eMatch = 0, sMatch = 0, hMatch = 0;
+
+                for(String affFile : cc.getAffectedFiles()){
+                    Set<String> analysedWords = postReflectAnalyser.extractAllFeatures(affFile, false);
+                    HashMap<Enum<MatchType>, List<String>> extractedWords = domainAnalyser.returnMatches(analysedWords);
+                    eMatch += extractedWords.get(MatchType.EXACT).size();
+                    sMatch += extractedWords.get(MatchType.SYNONYM).size();
+                    hMatch += extractedWords.get(MatchType.HYBRID).size();
+                }
+
+                matchTypeFreq.put(MatchType.EXACT, eMatch);
+                matchTypeFreq.put(MatchType.SYNONYM, sMatch);
+                matchTypeFreq.put(MatchType.HYBRID, hMatch);
+                commitMatchTypeFreq.put(cc.getId(), matchTypeFreq);
+            }
+        }
+
+        if(Params.GENERATE_FINAL_CLUSTERS){
+            for (Map.Entry<String, CommitContent> entry : commitContentSecMsgMap.entrySet()){
+                String cId = entry.getKey();
+                CommitContent cc = entry.getValue();
+
+                HashMap<Enum<MatchType>, List<String>> matchedWords = domainAnalyser.returnMatches(cc.getAnalysedMsg());
+                if (matchedWords.get(MatchType.EXACT).size() >= Params.FINAL_CLUSTER_EMATCH_THRESHOLD){
+                    FileHandler.saveToJson(Conf.FINAL_CLUSTERS + cId + ".json", cc);
+                }
+            }
+        }
+
     }
 
 }
