fe089b8ffeae8cf312bc196ec2f99297125db618
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-06 21:49:32 -0800

    cleaned and more configs and thresholds set

--- src/main/java/com/sarveshparab/clustering/SecurityClustering.java
+++ src/main/java/com/sarveshparab/clustering/SecurityClustering.java
@@ -5,6 +5,7 @@ import com.sarveshparab.analysers.domain.DomainAnalyser;
 import com.sarveshparab.analysers.domain.MatchType;
 import com.sarveshparab.analysers.semantic.SemanticAnalyser;
 import com.sarveshparab.config.Conf;
+import com.sarveshparab.config.Params;
 import com.sarveshparab.util.FileHandler;
 
 import java.util.*;
@@ -13,17 +14,20 @@ public class SecurityClustering {
 
     private ReflectionBasedAnalyser reflectionBasedAnalyser;
     private SemanticAnalyser semanticAnalyser;
+    private DomainAnalyser domainAnalyser;
     private List<String> filenames;
 
-    public SecurityClustering(SemanticAnalyser semanticAnalyser, ReflectionBasedAnalyser reflectionBasedAnalyser){
+    public SecurityClustering(SemanticAnalyser semanticAnalyser, ReflectionBasedAnalyser reflectionBasedAnalyser,
+                              DomainAnalyser domainAnalyser){
         this.semanticAnalyser = semanticAnalyser;
         this.reflectionBasedAnalyser = reflectionBasedAnalyser;
+        this.domainAnalyser = domainAnalyser;
         filenames=new ArrayList<>();
     }
 
-    public List<String> generateCluster(Set<String> smellyFiles, boolean loadPreGenerated){
+    public List<String> generateCluster(Set<String> smellyFiles){
 
-        if (loadPreGenerated && FileHandler.doesFileExists(Conf.SECURITY_CLUSTER_DUMP)){
+        if (Params.LOAD_SECURITY_CLUSTERS && FileHandler.doesFileExists(Conf.SECURITY_CLUSTER_DUMP)){
             return FileHandler.readListFromFile(Conf.SECURITY_CLUSTER_DUMP);
         }
 
@@ -31,18 +35,17 @@ public class SecurityClustering {
 
         for(String concernFile:smellyFiles) {
             Set<String> fileWords = reflectionBasedAnalyser.extractAllFeatures(concernFile, true);
-            //pass file words to domain analyser
-            DomainAnalyser domainAnalyser = new DomainAnalyser(semanticAnalyser);
-            //receive output from domain in a map
+
             HashMap<Enum<MatchType>, List<String>> wordMatches = domainAnalyser.returnMatches(fileWords);
 
             System.out.println(concernFile+","+
                     wordMatches.get(MatchType.EXACT).size()+","+wordMatches.get(MatchType.SYNONYM).size()
                     +","+wordMatches.get(MatchType.HYBRID).size()+","+fileWords.size());
 
-            if (wordMatches.get(MatchType.EXACT).size() >= 3 && wordMatches.get(MatchType.SYNONYM).size() >= 2){
+            if (wordMatches.get(MatchType.EXACT).size() >= Params.PRE_COMMIT_EMATCH_THRESHOLD
+                    && wordMatches.get(MatchType.SYNONYM).size() >= Params.PRE_COMMIT_SMATCH_THRESHOLD){
                 filenames.add(concernFile);
-            } else if(wordMatches.get(MatchType.HYBRID).size()>=10) {
+            } else if(wordMatches.get(MatchType.HYBRID).size() >= Params.PRE_COMMIT_HMATCH_THRESHOLD) {
                 filenames.add(concernFile);
             }
 
