3997cf4b82ef0aa8ffc74f096bdcfbe1c9f7ebc9
Sarvesh Parab <sarveshsparab@gmail.com>
2019-12-06 16:46:34 -0800

    misc params added

--- src/main/java/com/sarveshparab/config/Params.java
+++ src/main/java/com/sarveshparab/config/Params.java
@@ -2,4 +2,7 @@ package com.sarveshparab.config;
 
 public class Params {
     public static final boolean USE_PRE = true;
+
+    public static final boolean LOAD_SMELL_ANALYSIS = true;
+    public static final boolean LOAD_COMMIT_ANALYSIS = false;
 }
